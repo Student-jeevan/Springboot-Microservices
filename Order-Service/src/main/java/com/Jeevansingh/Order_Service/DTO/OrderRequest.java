d29724d166fd3d4d50a98d70cf7560f39881008c
jeevansingh <jeevanthitrya@gmail.com>
2025-09-04 09:32:17 +0530

    first commit

--- Order-Service/src/main/java/com/Jeevansingh/Order_Service/DTO/OrderRequest.java
+++
@@ -1,5 +0,0 @@
-package com.Jeevansingh.Order_Service.DTO;
-import java.math.BigDecimal;
-public record OrderRequest(Long id , String orderNumber , String SkuCode , BigDecimal price , Integer quantity) {
-
-}
