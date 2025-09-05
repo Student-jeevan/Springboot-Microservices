14c8892f3d72f584c57e125bf8c6f548962339c1
jeevansingh <jeevanthitrya@gmail.com>
2025-09-05 16:58:40 +0530

    first commit

--- Order-Service/src/main/java/com/Jeevansingh/Order_Service/repository/orderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Jeevansingh.Order_Service.repository;
-
-import com.Jeevansingh.Order_Service.model.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface orderRepository extends JpaRepository<Orders, Long> {
-
-}
